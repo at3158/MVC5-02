9d7f1a3383b61e46848ace1bec5ff04093bd9998
jain <at3158@hotmail.com>
2016-01-11 17:26:05 +0800

    add roles+menus+users
    add roles+menus+users

---
+++ MVC5-02/Migrations/201601110527028_InitialCreate.cs
@@ -0,0 +1,30 @@
+namespace MVC5_02.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserId = c.String(nullable: false, maxLength: 128),
+                        Email = c.String(),
+                        Password = c.String(),
+                        Name = c.String(),
+                        RegisterOn = c.DateTime(nullable: false),
+                        IsEnable = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ MVC5-02/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_02.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime RegisterOn { get; set; }
+
+        public bool IsEnable { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<Role> Roles { get; set; }
+    }
+
+    public class MyDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<MVC5_02.Models.Menu> Menus { get; set; }
+
+        public System.Data.Entity.DbSet<MVC5_02.Models.Role> Roles { get; set; }
+    }
+}
